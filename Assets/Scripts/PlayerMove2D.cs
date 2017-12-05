41b59eae8ca4a6f27a730b2a72aaa4dcc67c2441
ssaemo <jeontha@gmail.com>
2017-12-05 15:14:35 +0900

    player move

---
+++ Assets/Scripts/PlayerMove2D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  @brief 키보드 A,D를 입력받아 Player 좌우 Move
+ */
+class PlayerMove2D : MonoBehaviour
+{
+    public int SPEED = 10;
+    private Rigidbody compoRig;
+
+    void Start() { compoRig = GetComponent<Rigidbody>(); }
+
+    void Update()
+    {
+        Vector2 horiz = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+            horiz = -transform.right;
+        else if (Input.GetKey(KeyCode.D))
+            horiz = transform.right;
+
+        transform.Translate(horiz * SPEED * Time.deltaTime, Space.World);
+    }
+} // class

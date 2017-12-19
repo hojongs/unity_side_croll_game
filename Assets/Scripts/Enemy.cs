62ed4deffd717237aed1f5a592de2bd5164a8bb3
ssaemo <jeontha@gmail.com>
2017-12-19 22:01:57 +0900

    player hitted effect

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int DAMAGE = 1;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
